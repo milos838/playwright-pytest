85fd302e50c4cef3c08afe4e5fdb9df808eca8ca
milos.jancic <milos.jancic@inssio.com>
2026-08-19 09:41:43 +0200

    Add smoke test marker to login functionality test

--- Tests/test_EventHub_API_Login_functionality.py
+++ Tests/test_EventHub_API_Login_functionality.py
@@ -2,6 +2,7 @@ import pytest
 from playwright.sync_api import Playwright, expect, Page
 from Utils.apiBase import APIutils
 
+@pytest.mark.smoke
 def test_login_functionality_with_inserted_token(playwright: Playwright, browserInstance: Page, load_test_data):
     api_utils = APIutils()
     getToken = api_utils.getToken(playwright, load_test_data)
