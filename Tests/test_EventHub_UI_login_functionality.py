334bda2b9a71c232d7f95a13b8ff6ff4668cb91c
milos.jancic <milos.jancic@inssio.com>
2026-08-21 15:42:09 +0200

    Implement HomePage and LoginPage classes; refactor tests to utilize page objects

--- Tests/test_EventHub_UI_login_functionality.py
+++ Tests/test_EventHub_UI_login_functionality.py
@@ -1,11 +1,10 @@
 import pytest
 from playwright.sync_api import Page, expect
+from Pages.loginPage import LoginPage
 
 @pytest.mark.smoke
 def test_eventhub_ui_login_functionality(page: Page, load_test_data):
-    test_data = load_test_data
-    page.goto(test_data["url"])
-    page.locator("#email").fill(test_data["username"])
-    page.locator("#password").fill(test_data["password"])
-    page.locator("#login-btn").click()
-    expect(page.locator("#user-email-display")).to_have_text(test_data["username"])
+    login_page = LoginPage(page)
+    login_page.login(load_test_data)
+    login_page.login_validation(load_test_data)
+    
